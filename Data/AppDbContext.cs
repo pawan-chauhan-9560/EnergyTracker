f47ee7a143c04bbe36ecb3ebb404e266469680b4
PawanChauhan-del <pawanchauhan933@gmail.com>
2025-08-19 23:23:50 +0530

    Initial commit: .NET Core Energy Management backend project structure with DB context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EnergyManagementSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Add your DbSets here
+        //public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,6 @@
+﻿namespace EnergyManagementSystem.Services
+{
+    public class UserService
+    {
+    }
+}
